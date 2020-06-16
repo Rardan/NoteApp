b9b535a04171f09ff12c89e42ab48c3318201554
Rardan <przemolex01@gmail.com>
2020-06-16 19:35:53 +0200

    Dodaj pliki projektów.

---
+++ NoteApp/Data/NoteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NoteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteApp.Data
+{
+    public class NoteDbContext : IdentityDbContext<AppUser>
+    {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ NoteApp/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public AppUser User { get; set; }
+    }
+}
